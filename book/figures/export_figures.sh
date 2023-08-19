a8dc6dc2107b092b4e3feef2eef5978bdb587eee
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2023-08-19 16:35:02 +0200

    Change flowchart color (#185)
    * Change BG color to white
    
    * Fix flowcharts where graphics overlap with text
    
    * Increase border size
    
    * change arrow style
    
    * Allow standalone building of TikZ flowcharts
    
    Useful for reference or website etc..
    
    * Prevent manual hyphenation in TikZ diagrams
    
    A Bit of a manual tweak.. but worth it to look nicer :D
    
    * Add missing file for flowcharts
    
    ---------
    
    Co-authored-by: Cedric <ced@awase.ostal>

--- book/figures/export_figures.sh
+++ book/figures/export_figures.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-for i in *.pdf
-do
-  docker run -v $(pwd):/imgs dpokidov/imagemagick -density 900 -trim /imgs/$i -quality 100 /imgs/$i.png
-  rename 's/pdf.png/png/' *.png
-  rename 's/fig-/figure-/' *.png
-done
+#
 
+if [ "$#" -ne 1 ]; then
+    for i in *.pdf
+    do
+      target=figures/`basename $1 .png.pdf`.png
+      docker run -v $(pwd):/imgs dpokidov/imagemagick -density 900 -trim /imgs/$i -quality 100 /imgs/${target}
+      rename 's/fig-/figure-/' *.png
+    done
+else
+    target=figures/`basename $1 .png.pdf`.png
+    echo "Converting: " $1 " to: " ${target}
+    convert -density 900 -trim $1 -quality 100 ${target}
+fi
