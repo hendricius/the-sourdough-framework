b403440c048b7f59552b5f7850ad2c3d3566dcf3
cedounet <134267244+cedounet@users.noreply.github.com>
2023-09-15 07:37:52 +0100

    Remove sans serif ebook from makefile (#228)
    * Add missing pic dependency on quick_ebook target
    
    * Remove sans-serif ebooks from the build
    
    * Remove sans-serif ebook from website and README
    
    As per https://github.com/hendricius/the-sourdough-framework/issues/225
    point 2.  The reader program will allow to change the font, no point
    building it twice.
    
    * Remove sans_serif mk4 file

--- website/modify_build.rb
+++ website/modify_build.rb
@@ -493,7 +493,6 @@ class ModifyBuild
 
     <p class="noindent">
       EPUB: <a href="https://www.the-bread-code.io/book.epub">https://www.the-bread-code.io/book.epub</a><br>
-      EPUB (no serif): <a href="https://www.the-bread-code.io/book-sans-serif.epub">https://www.the-bread-code.io/book-sans-serif.epub</a>
     </p>
 
     <p class="noindent">
